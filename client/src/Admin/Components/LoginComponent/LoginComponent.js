d6a1f83515e4b599dbc2ca4fd1149feb6aa8b685
Nacho Torrella <nachotorrella@gmail.com>
2018-04-21 18:41:26 +0200

    added CSS styles

--- client/src/Admin/Components/LoginComponent/LoginComponent.js
+++ client/src/Admin/Components/LoginComponent/LoginComponent.js
@@ -1,45 +1,72 @@
 import React, { Component } from "react";
 
-import { auth } from '../../Services/FIREBASE';
+import { auth } from "../../Services/FIREBASE";
+
+import "./LoginComponent.css";
 
 class LoginComponent extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      user: '',
-      email: '',
-      password: ''
-    }
+      email: "",
+      password: ""
+    };
   }
 
   handleChange = e => {
     this.setState({
       [e.target.name]: e.target.value
-    })
-  }
+    });
+  };
 
-  handleSubmit = async (e) => {
-    e.preventDefault()
+  handleSubmit = async e => {
+    const { email, password } = this.state
+    e.preventDefault();
     try {
-      await auth.HandleLogin(this.state.email, this.state.password)
+      const response = await auth.HandleLogin(email, password);
+      console.log(`Logged as ${response.user.email}`)
+    } catch (err) {
+      console.log(err);
     }
-    catch (err) {
-      console.log(err)
-    }
-  }
+  };
+
+  handleResetPassword = async () => {
+    console.log('reset password...')
+  };
 
   render() {
     return (
       <div>
-        <header>
-          <h1>Login Page</h1>
-        </header>
-        <form onSubmit={this.handleSubmit} >
-          <input type="email" name="email" onChange={this.handleChange} value={this.state.email}/>
-          <input type="password" name="password" onChange={this.handleChange} value={this.state.password}/>
-          <button type="submit">login</button>
-
-        </form>
+        <div className="background-image" />
+        <div className="login-page">
+          <div className="form">
+            <form className="login-form" onSubmit={this.handleSubmit}>
+              <input
+                type="email"
+                placeholder="username"
+                name="email"
+                onChange={this.handleChange}
+                value={this.state.email}
+                required
+              />
+              <input
+                type="password"
+                placeholder="password"
+                name="password"
+                onChange={this.handleChange}
+                value={this.state.password}
+                required
+              />
+              <button type="submit">login</button>
+              <p className="message">
+                Has olvidado la contraseña?{" "}
+                <span onClick={this.handleResetPassword}>
+                  Solicita una nueva
+                </span>
+              </p>
+            </form>
+          </div>
+        </div>
       </div>
     );
   }
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import * as firebase from 'firebase';
 
 import App from './App';
 
